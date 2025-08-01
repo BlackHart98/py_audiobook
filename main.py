46de134981c13324f50aedcc83f5e211c946e4a1
Pius Arhanbhunde <pjacks419@gmail.com>
2025-08-01 18:24:27 +0100

    feat(): Finished the Sketch, still untested

--- main.py
+++ main.py
@@ -1,26 +1,88 @@
+import nltk
 import sys
 import typing as t
 import logging
+import fitz
 
+import numpy as np
+from bark.generation import (
+    generate_text_semantic,
+    preload_models,
+)
+from bark.api import semantic_to_waveform
+from bark import generate_audio, SAMPLE_RATE
+from scipy.io.wavfile import write as write_wav
 
-class PDFChunks:
-    order_list_of_paragraphs : t.List[str]
+# Mantra: It's all about the data not the structure, let the data layout guide design...
 
+nltk.download('punkt_tab')
 
-def chunk_pdf_files(pdf_file_path: str) -> t.List[str]:
+def get_pdf_content(pdf_file_path: str) -> str | None:
+    """
+    Parse PDF and extract the content in the PDF as str of texts 
+    Args:
+        pdf_file_path (str): The input text to be processed.
+    Returns:
+        str | None: The generated response.
+    """
     print("Chunking files...")
-    return None
+    try:
+        pdf_content: str = ""
+        with fitz.open(pdf_file_path) as f:
+            doc: t.Any = f
+            # f.close()
+            for page in doc:
+                pdf_content += f"{page.get_text()}\n"
+        return pdf_content
+    except:
+        return None
+
+
+def generate_sentences(pdf_content: str) -> t.List[str]:
+    _pdf_content = pdf_content.replace("\n", " ").strip()    
+    return nltk.sent_tokenize(_pdf_content) 
+
+
+def generate_audio_file(
+    sentence_list: t.List[str], 
+    device: str="cpu", 
+    tts_model: str="v2/en_speaker_6",
+    gen_temp = 0.6,
+) -> None:
+    """
+    Generate audio files from pdf_content
+    Args:
+        pdf_content (str): The input text to be processed.
+    Returns:
+        t.Any | None: The generated audio file.
+
+    """
+    silence = np.zeros(int(0.25 * SAMPLE_RATE))
+    pieces = []
+    for sentence in sentence_list:
+        semantic_tokens = generate_text_semantic(
+            sentence,
+            history_prompt=tts_model,
+            temp=gen_temp,
+            min_eos_p=0.05,
+        )
+        audio_array = semantic_to_waveform(semantic_tokens, history_prompt=tts_model)
+        pieces += [audio_array, silence.copy()]
+    write_wav(f"output/sample.wav", SAMPLE_RATE, audio_array)
+
 
 
 
 def main(argv: t.List[str]) -> int:
-    if (len(argv) != 3):
+    if len(argv) != 2:
         logging.error("Invalid command.")
         print("Usage: python main.py <file_path>")
         return 1
     else:
         file_path: str = argv[1]
-        pdf_chunks : t.List[str] = chunk_pdf_files(file_path)
+        pdf_chunks: str | None = get_pdf_content(file_path)
+        sentence_list: t.List[str] = generate_sentences(pdf_chunks) if pdf_chunks else []
+        generate_audio_file(sentence_list)
         return 0
 
 
